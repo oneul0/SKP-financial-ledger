1f22e7cc4eb165a3fae2579fea5e7596a0dfdaf8
unknown <sb5811c98@gmail.com>
2024-06-11 09:45:43 +0900

    병철

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
-app.mount('#app')
+app.use(router);
+app.mount("#app");
