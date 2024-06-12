11f6882ec80ade5ad805ad6da537b0067f1a289e
yyyygmin <ygmin1004@naver.com>
2024-06-12 16:57:51 +0900

    Feat: Add Login, SignUp, Setting

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,19 @@
 import { createWebHistory, createRouter } from 'vue-router'
 
+import home from '../views/HomeView.vue';
+import login from '../views/LoginView.vue';
+import signUp from '../views/SignUpView.vue';
+import setting from '../views/SettingView.vue';
+
+
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-
+        { path: '/login', name:'login', component: login},
+        { path: '/', name:'home', component: home},
+        { path: '/signUp', name:'signUp', component: signUp},
+        { path: '/setting', name:'setting', component: setting}
     ]
 });
 
