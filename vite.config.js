ade23c47bf759dcd5222f60ce660874d92ca73c8
oneul0 <raeque@naver.com>
2024-06-22 16:41:32 +0900

    vite.config.js 수정

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { fileURLToPath, URL } from "url";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/actions-test/',
   css: {
     preprocessorOptions: {
       css: {
