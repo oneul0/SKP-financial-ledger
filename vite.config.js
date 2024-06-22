af36f355bdde649938dfddb5c77701ef48fda344
oneul0 <raeque@naver.com>
2024-06-22 16:42:09 +0900

    vite.config.js 수정

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { fileURLToPath, URL } from "url";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/actions-test/',
+  base: '/SKP-financial-ledger/',
   css: {
     preprocessorOptions: {
       css: {
